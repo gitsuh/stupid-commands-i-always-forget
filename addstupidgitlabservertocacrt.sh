c2fec563b30e62b24421593d8834e77ffd5b857a
gitsuh <21348752+gitsuh@users.noreply.github.com>
2019-04-13 18:56:30 -0400

    Update addstupidgitlabservertocacrt.sh

--- addstupidgitlabservertocacrt.sh
+++ addstupidgitlabservertocacrt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ $1 -eq 0 ]] ; then
-    echo 'Missing gitlab url. (e.g.  ./addstupidgitlabservertocacrt.sh stupidgitlab.com'
+    echo 'Missing gitlab url. (e.g.  ./addstupidgitlabservertocacrt.sh stupidgitlab.com)'
     exit 1
 fi
 
 if [[ $2 -eq 0 ]] ; then
-    echo 'Missing gitlab url port. (e.g.  ./addstupidgitlabservertocacrt.sh stupidgitlab.com 443'
+    echo 'Missing gitlab url port. (e.g.  ./addstupidgitlabservertocacrt.sh stupidgitlab.com 443)'
     exit 1
 fi
 
